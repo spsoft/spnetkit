aa93b1a26fbe98f013f9ae5944a1a4db2395c64f
stephen.nil <stephen.nil@f3fa06a4-2043-0410-a5e1-9d8471232586>
2008-01-11 15:14:55 +0000

    add remove for stringlist

--- spnetkit/spnkutils.hpp
+++ spnetkit/spnkutils.hpp
@@ -37,6 +37,7 @@ public:
 	int directAppend( char * value );
 	int append( const char * value, int len = 0 );
 	const char * getItem( int index ) const;
+	int remove( int index );
 	char * takeItem( int index );
 	int seek( const char * sample ) const;
 	char * getMerge( int * len = 0, const char * sep = 0 );
