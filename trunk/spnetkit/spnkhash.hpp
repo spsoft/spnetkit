b88a209f7aec2faa0448b550db5c068e806c7f3c
stephen.nil <stephen.nil@f3fa06a4-2043-0410-a5e1-9d8471232586>
2008-02-17 07:30:30 +0000

    add Fowler-Noll-Vo hash function

--- trunk/spnetkit/spnkhash.hpp
+++ trunk/spnetkit/spnkhash.hpp
@@ -13,6 +13,21 @@ class SP_NKHash {
 public:
 	static uint32_t crc32( const char * key, size_t len );
 
+	/// Implements a 64 bit long Fowler-Noll-Vo hash
+	static uint64_t fnv64( const char * key, size_t len );
+
+	static uint64_t fnv64a( const char * key, size_t len );
+
+	static uint32_t fnv32( const char * key, size_t len );
+
+	static uint32_t fnv32a( const char * key, size_t len );
+
+	static const uint64_t FNV_64_INIT= 0xcbf29ce484222325LL;
+	static const uint64_t FNV_64_PRIME= 0x100000001b3LL;
+
+	static const uint32_t FNV_32_INIT= 2166136261UL;
+	static const uint32_t FNV_32_PRIME= 16777619;
+
 private:
 	SP_NKHash();
 };
